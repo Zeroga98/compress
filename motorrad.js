f4bafa609fa266b3a9cf4e657a4ca8778ede60ed
Zeroga98 <sebastianrojasgarzon@hotmail.com>
2020-07-22 10:37:40 -0500

    mini, bmw, motorrad

---
+++ motorrad.js
@@ -0,0 +1,46 @@
+console.log("======= iniciando ==========")
+const { compress } = require("compress-images/promise");
+const { async } = require("q");
+try {
+    let count = [];
+    const processImages = async () => {
+        const result = await compress({
+            source: __dirname + "/motorrad/**/*.{jpg,JPG,jpeg,JPEG}",
+            destination: __dirname + "/buildMotorrad/",
+            enginesSetup: {
+                jpg: { engine: "webp", command: false },
+                png: { engine: false, command: false },
+                svg: { engine: false, command: false },
+                gif: { engine: false, command: false }
+            }
+        });
+        const { statistics, errors } = result;
+        console.log(errors, "primer intento")
+        if (errors.length) {
+            console.log(errors, "Segundo intento")
+            const resolve = await Promise.all(errors.map(async (folder) => {
+                const resultTwo = await compress({
+                    source: folder.input,
+                    destination: __dirname + "/buildMotorrad/",
+                    enginesSetup: {
+                        jpg: { engine: "webp", command: false },
+                        png: { engine: false, command: false },
+                        svg: { engine: false, command: false },
+                        gif: { engine: false, command: false }
+                    }
+                });
+                return resultTwo.errors
+             /*    const { statistics, errorsTwo } = resultTwo;
+                if (errorsTwo.length) {
+                    count.push(errors[0])
+                } */
+            }))
+            if (resolve.length) {
+                console.log(count, "no se pudo guardar", resolve.length);
+            }
+        }
+    };
+    processImages();
+} catch (error) {
+    throw error
+}
