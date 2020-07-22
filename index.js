d7f679b8ab290645deb6889c3e4475c6e2431b89
Zeroga98 <sebastianrojasgarzon@hotmail.com>
2020-07-22 07:56:38 -0500

    unix

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
-console.log("======= iniciando compresión ==========")
+console.log("======= iniciando compresión ==========", __dirname)
 const compress_images = require("compress-images")
 try {
 
     compress_images(
-        "../bmw/**/*.{jpg,JPG,jpeg,JPEG}",
-        "buildBmw/img/",
-        { compress_force: false, statistic: true, autoupdate: true },
-        false,
-        { jpg: { engine: "webp", command: false } },
-        { png: { engine: false, command: false } },
-        { svg: { engine: false, command: false } },
-        { gif: { engine: false, command: false } },
-        function (err) {
-            if (err) {
-                console.error("ERROR BMW:", err);
-            }
-        }
-    );
-
-    compress_images(
-        "../mini/**/*.{jpg,JPG,jpeg,JPEG}",
-        "buildMini/img/",
+        __dirname + "/mini/**/*.{jpg,JPG,jpeg,JPEG}",
+        __dirname + "/buildMini/img/",
         { compress_force: false, statistic: true, autoupdate: true },
         false,
         { jpg: { engine: "webp", command: false } },
@@ -35,8 +19,8 @@ try {
     );
 
     compress_images(
-        "../motorrad/**/*.{jpg,JPG,jpeg,JPEG}",
-        "buildMotorrad/img/",
+        __dirname + "/bmw/**/*.{jpg,JPG,jpeg,JPEG}",
+        __dirname + "/buildBmw/img/",
         { compress_force: false, statistic: true, autoupdate: true },
         false,
         { jpg: { engine: "webp", command: false } },
@@ -45,12 +29,26 @@ try {
         { gif: { engine: false, command: false } },
         function (err) {
             if (err) {
-                console.error("ERROR BMWMOTORRAD:", err);
+                console.error("ERROR MINI:", err);
             }
         }
     );
-
-
+    compress_images(
+        __dirname + "/motorrad/**/*.{jpg,JPG,jpeg,JPEG}",
+        __dirname + "/buildMotorrad/img/",
+        { compress_force: false, statistic: true, autoupdate: true },
+        false,
+        { jpg: { engine: "webp", command: false } },
+        { png: { engine: false, command: false } },
+        { svg: { engine: false, command: false } },
+        { gif: { engine: false, command: false } },
+        function (err) {
+            if (err) {
+                console.error("ERROR MINI:", err);
+            }
+        }
+    );
+    
 } catch (error) {
     throw error
 }
